3bd67992e77118c32e1e265ea98a753b320fd9d9
falgunishobnom <fshobnom.io@gmail.com>
2024-08-13 11:50:27 +0600

    First commit after git init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#ABBAAB",
+      },
+      fontFamily: {
+        header: ["Plus Jakarta Sans"],
+      },
+    },
+  },
+  plugins: [],
+};
